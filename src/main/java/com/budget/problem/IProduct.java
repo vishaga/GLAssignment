9b1404a65adf2a2db7186b3517eae6685a00f3c8
Gaurav Vishal <gaurav.vishal@accolite.com>
2021-02-25 00:44:46 +0530

    Initial Commit

--- src/main/java/com/budget/problem/IProduct.java
+++
@@ -1,7 +0,0 @@
-package com.budget.problem;
-
-public abstract class IProduct {
-
-	public abstract void printService(Product p);
-
-}
--- src/main/java/com/budget/problem/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.budget.problem;
-
-public class ProductService {
-
-	public void printService(Product p) {
-		System.out.println(p.getId());
-	}
-
-}
